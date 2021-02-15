812abe06d5f09b2083e8387c615cdf672c333094
ch_p <webserv2010@yandex.ru>
2021-02-15 23:18:47 +0300

    Pi apdate

--- pi_read.py
+++ pi_read.py
@@ -2,5 +2,9 @@ filename = 'pi_digits.txt'
 
 with open(filename) as file_object:
     lines = file_object.readlines()
-    for line in lines:
-        print(line.rstrip())
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
