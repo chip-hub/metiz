53d40941ed8dfe62f832dc3dbcef7501634f8951
ch_p <webserv2010@yandex.ru>
2021-02-16 16:50:03 +0300

    Edit person

--- persona.py
+++ persona.py
@@ -16,6 +16,14 @@ class User():
         self.login_attempts += 1
 
     def reset_login_attempts(self):
+        self.login_attempts = 0
 
 user = User('Alex', 'Priv', age = '19', adress = 'Bryansk')
 user.describe_user()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+print(f"Количество посещений {user.person_data['first_name']} - {user.login_attempts}")
+
+user.reset_login_attempts()
+print(f"Сброшено количество посещений {user.person_data['first_name']} - {user.login_attempts}")
